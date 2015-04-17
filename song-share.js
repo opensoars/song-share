8960ede08b67ebc949d8c03f6fe7d69d90a1e46c
unknown <sam_1700@hotmail.com>
2015-04-17 12:51:21 +0200

    New clean work enviroment

--- song-share.js
+++ song-share.js
@@ -1,86 +1,23 @@
-/**
- * Globals
- */
-var HTTP_PORT = 3333;
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
 
-/**
- * Mongo collections
- */
-var Songs = new Mongo.Collection('songs');
-
-
-if(Meteor.isClient){
-  Meteor.subscribe('songs');
-
-  Template.upload.events({
-    'change input': function (evt){
-      var file = evt.target.files[0];
-
-      var worker = new Worker('readAsDataURL.js');
-      
-      worker.onmessage = function (msg){
-        var song = msg.data[0];
-        Meteor.call('addSong', song);
-      };
-
-      worker.postMessage([file]);
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
     }
   });
 
-
-  /**
-   * Accounts config
-   */
-  Accounts.ui.config({
-    passwordSignupFields: "USERNAME_ONLY"
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
   });
 }
 
-/**
- * Meteor methods
- */
-Meteor.methods({
-  addSong: function (song){
-    HTTP.call('POST', 'http://127.0.0.1:' + HTTP_PORT, { data: song, },
-      function (err, res){
-        if(err) return console.log(err);
-        console.log('http callback');
-        console.log(res);
-      }
-    );
-  }
-});
-
-if(Meteor.isServer){
-
-  Meteor.startup(function (){
-
-    /**
-     * Server npm requirements
-     */
-    var http = Npm.require('http'),
-        fs = Npm.require('fs');
-
-
-    /**
-     * HTTP server used for storing base64 songs
-     */
-    http.createServer(function (req, res){
-      var b = ''; req.on('data', function (c){ b += c; });
-
-      req.on('end', function (){
-        console.log(b.length);
-      });
-
-      res.writeHead(200, {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers':
-            'Origin, X-Requested-With, Content-Type, Accept'
-      });
-      res.end(JSON.stringify( {status: 'ok'} ));
-
-    }).listen(HTTP_PORT);
-
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
   });
 }
